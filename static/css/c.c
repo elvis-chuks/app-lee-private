4f361ae16bb11a26e813c06b0c8feadf6082f6ea
Elvis <celvischuks@gmail.com>
2019-01-26 20:00:30 +0100

    added dashboard styling

---
+++ static/css/c.c
@@ -0,0 +1,66 @@
+.sidebar{
+  position: absolute;
+width: 249px;
+height: 100%;
+left: 0px;
+top: 0px;
+
+background: #243447;
+}
+.sidebar {
+  color:white;
+}
+.sidebar .buttons #btn1{
+position: absolute;
+width: 154px;
+height: 35px;
+left: 48px;
+top: 100px;
+color:white;
+background: #243447;
+border: 1px solid #fff;
+box-sizing: border-box;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;
+cursor: crosshair;
+}
+.sidebar .buttons #btn2{
+  position: absolute;
+width: 154px;
+height: 35px;
+left: 50px;
+top: 158px;
+color:white;
+background: #243447;
+border: 1px solid #fff;
+box-sizing: border-box;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;
+
+}
+.sidebar .buttons #btn3{
+  position: absolute;
+width: 154px;
+height: 35px;
+left: 48px;
+top: 221px;
+color:white;
+background: #243447;
+border: 1px solid #fff;
+box-sizing: border-box;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;
+}
+.sidebar .buttons #btn4{
+  position: absolute;
+width: 154px;
+height: 35px;
+left: 51px;
+top: 578px;
+color:white;
+background: #243447;
+border: 1px solid #fff;
+box-sizing: border-box;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;
+}
